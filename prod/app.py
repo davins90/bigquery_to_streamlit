6d8d6576f7104f0c3088daf4726e5c4cc7199241
dani <daniele.davino@live.it>
2023-05-17 17:42:52 +0200

    edit ids

--- prod/app.py
+++ prod/app.py
@@ -2,18 +2,21 @@ import streamlit as st
 from google.cloud import bigquery
 import pandas_gbq
 
-# Google Cloud Project ID and dataset ID
+# Google Cloud Project ID
 project_id = "bigquery-to-streamlit"
-dataset_id = "bigquery-to-streamlit.yt_db"
+
+# Dataset ID and Table ID
+dataset_id = "yt_db"
+table_id = "df"
 
 # Start a BigQuery client
-client = bigquery.Client(project=project_id)
+# client = bigquery.Client(project=project_id)
 
 # SQL query to select data from the table
 sql = """
 SELECT * 
 FROM `{}.{}`
-""".format(dataset_id, "bigquery-to-streamlit.yt_db.df")
+""".format(dataset_id, table_id)
 
 # Load data into a DataFrame
 df = pandas_gbq.read_gbq(sql, project_id=project_id)
