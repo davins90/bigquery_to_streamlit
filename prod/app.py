cdeb2144477d8aada20227707649b0460d7a0f5e
dani <daniele.davino@live.it>
2023-05-22 13:16:57 +0200

    edit python code for avoiding error nan in istutorial

--- prod/app.py
+++ prod/app.py
@@ -41,7 +41,7 @@ st.dataframe(df.head(10))
 st.markdown("## 2) Filtering Table")
 
 # Filter for considering only not analyzed video
-df['isTutorial'] = df['isTutorial'].fillna('empty')
+df['isTutorial'] = df['isTutorial'].astype(str).fillna('empty')
 df_filtered = df[(df['isTutorial']!="Y") & (df['isTutorial']!="N")]
 
 country = st.selectbox("Filter Table for the country desidered.",("DE","IT","GB"),index=2)
